40b3be69f9fb918f4062fd68867df9e1c6263b52
Uzair <uzairarifjokhio@gmail.com>
2023-07-21 18:49:25 +0500

    modification to carmodel

--- cpp project parking 1/design.h
+++ cpp project parking 1/design.h
@@ -6,19 +6,6 @@ using namespace std;
 
 /* These function are very helpfull for making the GUI look good :) */
 
-
-
-void car_md() {
-    /*A car model design */
-    cout << "   _____  " << endl;
-    cout << "  /|||_\\`._ " << endl;
-    cout << " (   _    _ _\\ " << endl;
-    cout << " =`-(_)--(_)-' " << endl;
-    cout << "  Car Wala Bhai"; // You can change the name..
-}
-
-
-
 void star_start() {
     cout << "\n\n\n\t\t\t\t***************************************************************" << endl;
 }
@@ -31,8 +18,8 @@ void doubledash() {
 	cout << "\n\t\t===============================================";
 }
 
-// Function to set the text color
-void setColor(int color)
+// Function to set the text color       
+void setColor(int color)            
 {
     cout << "\033[" << color << "m";
 }
@@ -41,4 +28,27 @@ void setColor(int color)
 void resetColor()
 {
     cout << "\033[0m";
+}
+
+
+
+void car_md() {
+    /*A car model design */
+    // Set car color to orange (33 is the ANSI escape)
+    setColor(33);
+    /*A car model design */
+    cout << "   _____  " << endl;
+    cout << "  /|||_\\`._ " << endl;
+    cout << " (   _    _ _\\ " << endl;
+    cout << " =`-(_)--(_)-' " << endl;
+
+    // Reset color to default
+    resetColor();
+
+    // Set name color to cyan (33 is the ANSI escape )
+    setColor(36);
+    cout << "  Car Wala Bhai" << endl; // You can change the name..
+
+    // Reset color to default
+    resetColor();
 }
